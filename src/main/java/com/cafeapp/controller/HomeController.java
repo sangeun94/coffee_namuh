0a94fc27c6ee76ae9cb57b810b4241596af94e5a
honeyfive <devhoneyfive@gmail.com>
2024-01-31 17:33:34 +0900

    푸터 완성 및 메뉴(음료) html 제작 시작

--- src/main/java/com/cafeapp/controller/HomeController.java
+++ src/main/java/com/cafeapp/controller/HomeController.java
@@ -1,5 +1,14 @@
 package com.cafeapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
 
+	
+	@RequestMapping("/menu/menu_drink")
+	public String test() {
+		return "menu/menu_drink";
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/menu/menu_drink.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>COFFEE NAMUH</title>
+</head>
+<body>
+<h1>메뉴 페이지</h1>
+<%@ include file="/WEB-INF/views/footer/footer.jsp"%>
+</body>
+</html>
